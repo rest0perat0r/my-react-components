470bc7a6aa3d874c96e34f9de9fd8f5516863e90
rest0perat0r <restoperatorshafi@gmail.com>
2023-02-19 23:56:19 +0600

    modified the function component Greet.js to practice how props and children works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-
+import Greet from "./components/Greet";
 
 function App() {
   return (
     <div className="App">
- 
+      <Greet name="Shams" age={30}>
+        Scientist
+      </Greet>
+      <Greet name="Akash" age={10}>
+        Teacher
+      </Greet>
+      <Greet name="Bipul" age={20}>
+        Businessman
+      </Greet>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Greet = () => <h1>Hello Everyone</h1>;
+const Greet = (props) => {
+  /*   return (
+    <div>
+      <h1>I am {props.name}</h1>
+      <p>My age is: {props.age}</p>
+    </div>
+  ); */
+  const { name, age } = props; // using destructure technique
+  return (
+    <div>
+      <h1>I am {name}</h1>
+      <p>My age is: {age}</p>
+      <button>Profession: {props.children}</button>
+    </div>
+  );
+};
 
 export default Greet;
